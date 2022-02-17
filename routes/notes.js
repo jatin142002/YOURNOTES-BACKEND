6586b588a4063d7e0b90fafb496b1214619e5a23
Jatin Mehra <mehrajatin142002@gmail.com>
2022-02-17 19:50:38 +0530

    End point for updating the note is added

--- routes/notes.js
+++ routes/notes.js
@@ -65,4 +65,43 @@ router.post('/addnote' , fetchuser , [
 });
 
 
+// ROUTE 3 : Update an existing note using : PUT "api/notes/updatenote" . Login required
+router.put('/updatenote/:id' , fetchuser , async (req , res)=>{
+
+    const {title,description,tag} = req.body;
+    //Create a newNote object
+
+    const newNote = {};
+    if(title)
+    {
+        newNote.title = title;
+    }
+    if(description)
+    {
+        newNote.description = description;
+    }
+    if(tag)
+    {
+        newNote.tag = tag;
+    }
+
+    // Find the note to be upadted and update it..
+    let note = await Notes.findById(req.params.id);
+
+    if(!note)
+    {
+        return res.status(404).send("Not Found !!");
+    }
+
+    if(note.user.toString() !== req.user.id)
+    {
+        return res.status(401).send("Not Allowed !!");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id , {$set : newNote} , {new : true});
+    res.json({note});
+
+});
+
+
 module.exports = router;
